010fd2f74cb7be539254959a83a34c39ccc9a88e
joaom98 <joaomanoellima98@gmail.com>
2020-06-23 11:32:56 -0300

    Adapted the animation file

--- jogo/animacao.js
+++ jogo/animacao.js
@@ -1,9 +1,32 @@
 class Animacao {
-    constructor( imagem, qualSprite, larguraRender, alturaRender, larguraSprite, AlturaSprite )  {
-    this.imagem = imagem;
-    this.qualSprite = qualSprite;
-    this.larguraRender = larguraRender;
-    this.alturaRender = alturaRender;
-    this.AlturaSprite = AlturaSprite; 
+    constructor( imagem, qualSprite, xRender, yRender, larguraRender, alturaRender, larguraSprite, alturaSprite )  {
+        this.imagem = imagem;
+        this.qualSprite = qualSprite;
+        this.xRender = xRender;
+        this.yRender = height - yRender;
+        this.larguraRender = larguraRender;
+        this.alturaRender = alturaRender;
+        this.larguraSprite = larguraSprite;
+        this.alturaSprite = alturaSprite;
+        this.frameAtual = 0; 
+    }
+    
+    exibe(){
+        let x = this.frameAtual % 4 * this.larguraSprite;
+        let y = Math.floor(this.frameAtual / 4) * this.alturaSprite;
+        
+        image(this.imagem, this.xRender, this.yRender, this.larguraRender, this.alturaRender, x, y, this.larguraSprite, this.alturaSprite);
+        
+        this.anima();
+    }
+    
+    anima(){
+        this.frameAtual++;
+        
+        this.frameAtual = frameAtual % 15;
+        //if (this.frameAtual > 15) {
+        //    this.frameAtual = 0;
+        //}
+    }
     
 }
