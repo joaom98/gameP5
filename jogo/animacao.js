86b8c7681b554b56eb932e87f7df65a9480a82ba
joaom98 <joaomanoellima98@gmail.com>
2020-06-23 11:16:26 -0300

    Added animation file

---
+++ jogo/animacao.js
@@ -0,0 +1,9 @@
+class Animacao {
+    constructor( imagem, qualSprite, larguraRender, alturaRender, larguraSprite, AlturaSprite )  {
+    this.imagem = imagem;
+    this.qualSprite = qualSprite;
+    this.larguraRender = larguraRender;
+    this.alturaRender = alturaRender;
+    this.AlturaSprite = AlturaSprite; 
+    
+}
