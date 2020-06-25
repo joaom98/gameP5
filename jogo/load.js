c48d17eb09c158bfe98273ad369d9b92df919aed
joaom98 <joaomanoellima98@gmail.com>
2020-06-25 11:09:28 -0300

    adicionados arquivos de organizacao

---
+++ jogo/load.js
@@ -0,0 +1,13 @@
+function preload(){
+    imagemCenario = loadImage("assets/imagens/cenario/floresta.png");
+    imagemGameOver = loadImage("assets/imagens/assets/game-over.png");
+    
+    imagemPersonagem = loadImage("assets/imagens/personagem/correndo.png");
+    imagemInimigoGota = loadImage("assets/imagens/inimigos/gotinha.png");
+    imagemTroll = loadImage("assets/imagens/inimigos/troll.png");
+    imagemGotaVoadora = loadImage("assets/imagens/inimigos/gotinha-voadora.png");
+    
+    
+    trilhaSonora = loadSound("assets/sons/trilha_jogo.mp3");
+    somPulo = loadSound("assets/sons/somPulo.mp3");
+}
---
+++ jogo/variaveis.js
@@ -0,0 +1,23 @@
+let imagemCenario;
+let imagemPersonagem;
+let imagemGameOver;
+
+let pontuacao = new Pontuacao();
+
+let inimigoAtual = 0;
+const inimigos = [];
+
+let inimigoGota;
+let imagemInimigoGota;
+
+let inimigoTroll;
+let imagemTroll;
+
+let inimigoVoador;
+let imagemGotaVoadora;
+
+let cenario;
+let trilhaSonora;
+let somPulo;
+
+let personagem;
